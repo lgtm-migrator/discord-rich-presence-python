978b71be50023fd2e06d088024a609f07fd35bf8
Enz <33209196+gitcloneguy@users.noreply.github.com>
2020-10-19 09:14:36 -0700

    b e t t e r

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ while True:
     cpu_per = round(psutil.cpu_percent(),1) 
     mem = psutil.virtual_memory()
     mem_per = round(psutil.virtual_memory().percent,1)
+    try:
+        RPC.update(large_image="taskmanager", large_text="updates every 15 seconds", state="RAM: "+str(mem_per)+"%", details="CPU: "+str(cpu_per)+"%")
+    except Exception:
+        print('Something went wrong while updating rich Presence')
+    else:
+        print('Rich Presence updated.')
     time.sleep(15)
 
 while True: 
