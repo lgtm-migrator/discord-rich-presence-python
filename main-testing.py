628935a29eac9911797fdaf9db7196f2849837c6
Enz <33209196+gitcloneguy@users.noreply.github.com>
2020-10-19 10:49:55 -0700

    what pop101?

--- main-testing.py
+++ main-testing.py
@@ -10,12 +10,12 @@ print(200 <= requests.get('http://discord.com').status_code < 300)
 
 
  
-    cpu_per = round(psutil.cpu_percent(),1) 
-    mem = psutil.virtual_memory()
-    mem_per = round(psutil.virtual_memory().percent,1)
-    try:
-        RPC.update(large_image="taskmanager", large_text="updates every 15 seconds", state="RAM: "+str(mem_per)+"%", details="CPU: "+str(cpu_per)+"%")
-    except Exception:
-        print('Something went wrong while updating rich Presence')
-    else:
-        print('Rich Presence updated.')
+cpu_per = round(psutil.cpu_percent(),1) 
+mem = psutil.virtual_memory()
+mem_per = round(psutil.virtual_memory().percent,1)
+try:
+    RPC.update(large_image="taskmanager", large_text="updates every 15 seconds", state="RAM: "+str(mem_per)+"%", details="CPU: "+str(cpu_per)+"%")
+except Exception:
+    print('Something went wrong while updating rich Presence')
+else:
+    print('Rich Presence updated.')
